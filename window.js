91dc904593de8dcde4f819af9f591f7e6e11d039
Park <confirm1456@gmail.com>
2018-09-11 12:13:23 +0900

    chat 1.0.0

---
+++ window.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function initWindow() {
+    mainWindow = new BrowserWindow({
+        width: 960,
+        height: 660,
+        minWidth: 960,
+        minHeight: 660,
+        frame: false,
+        show: false,
+        icon: path.join(__dirname, 'src/resources/icons/48x48.png'),
+        backgroundColor: '#fff'
+    });
+
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show();
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', initWindow);
+app.on('activate', () => {
+    if (mainWindow === null) {
+        initWindow();
+    }
+});
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
